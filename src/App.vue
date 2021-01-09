360b38c043276b440b480d82ea9fb4e8397ff1c4
Marcelo Oliveira <marcelotk_15@hotmail.com>
2021-01-09 11:42:47 -0300

    📦 NEW: project base

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Operand CRUD - VueJS</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+</style>
